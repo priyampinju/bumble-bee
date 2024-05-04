b3a0b4c8c1c6f123228d0e2bcc654ff722da3826
priyampinju <pinjupriyam1826@gmail.com>
2024-05-04 16:18:14 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,19 @@
+import cafe from "../assets/img/23.jpg";
+
 const About = () => {
   return (
-    <div className="text-white text-3xl flex justify-center items-center py-5 mx-auto">
-      <div className="w-1/2">sbdhf</div>
-      <div className="">sdkjsnf</div>
+    <div className="text-white text-3xl py-10 mx-auto">
+      <h1 className="w-full text-center pb-5 font-rochester">About our Cafe</h1>
+      <div className="w-full flex items-center">
+        <div className="w-1/2 pl-9">
+          {" "}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+          exercitationem!
+        </div>
+        <div className="">
+          <img src={cafe} alt="cafe" width={600} />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,11 @@
 import coffecup from "../assets/img/29.jpg";
+import coffeecup from "../assets/img/coffeeccup.jpg";
+import coffecup1 from "../assets/img/coffecup1.jpg";
+
 const Menu = () => {
   return (
     <div>
-      <img src={coffecup} alt="" />
+      <img src={coffecup1} alt="coffee" width={600} />
     </div>
   );
 };
